b6ef9eca776a8ec350f9a76f15db1729a0eb8761
Mark Nelson <mark.x.nelson@oracle.com>
2015-04-21 13:10:50 +1000

    4.0

---
+++ app/src/main/java/com/samuiinteractive/ati/BookmarkOpenHelper.java
@@ -0,0 +1,36 @@
+package com.samuiinteractive.ati;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BookmarkOpenHelper  extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 5;
+    private static final String DATABASE_NAME = "bookmarks";
+    public static final String BOOKMARKS_TABLE_NAME = "bookmarks";
+    public static final String TITLE_COLUMN_NAME = "title";
+    public static final String URL_COLUMN_NAME = "url";
+    private static final String BOOKMARKS_TABLE_CREATE =
+                "CREATE TABLE " + BOOKMARKS_TABLE_NAME + " (" +
+                TITLE_COLUMN_NAME + " TEXT, " +
+                URL_COLUMN_NAME + " TEXT);";
+
+    BookmarkOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(BOOKMARKS_TABLE_CREATE);
+        //db.execSQL("INSERT INTO BOOKMARKS (TITLE, URL) VALUES ('TEST BOOKMARK','file:///android_asset/tipitaka/an/an01/an01.047.than.html')");
+    }
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		if (oldVersion < 5) {
+            // do upgrade (nothing to do)
+        }
+	}
+}
